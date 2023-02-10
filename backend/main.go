60c61eeb6add68ea7c02eb25710c0859b4624db6
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-02-10 19:38:38 -0300

    running server on localhost with one endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"pdfPro/handler"
+)
 
 func main() {
-	fmt.Println("Hello World! Fev 8, 2023.")
+	port, ok := os.LookupEnv("PORT")
+	if ok != true {
+		log.Fatalf("The env variable %s is not set", "PORT")
+	}
+	http.HandleFunc("/api/v1/genPdf", handler.HandlePdfGen)
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
 }
