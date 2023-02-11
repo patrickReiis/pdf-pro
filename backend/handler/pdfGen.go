d3ee49b7d4a18e694ec12c747aef1e36f8933ded
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-02-11 19:19:04 -0300

    Read request body and parse it to JSON

--- backend/handler/pdfGen.go
+++ backend/handler/pdfGen.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,10 +11,27 @@ func HandlePdfGen(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprint(w, "Only POST method allowed")
 		return
 	}
 
-	fmt.Fprint(w, "Hello from pdf gen")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Error reading request body")
+		return
+	}
+
+	var jsonData map[string]interface{}
+
+	err = json.Unmarshal(body, &jsonData)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Send valid JSON")
+		return
+	}
+
+	fmt.Fprintf(w, "Hello from pdf gen %v", jsonData)
 }
