cacb48b5730479e35d8a718697a06966497aa1bd
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-02-11 18:23:27 -0300

    Only POST method allowed in pdfGen endpoint

--- backend/handler/pdfGen.go
+++ backend/handler/pdfGen.go
@@ -7,5 +7,12 @@ import (
 
 func HandlePdfGen(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		fmt.Fprint(w, "Only POST method allowed")
+		return
+	}
+
 	fmt.Fprint(w, "Hello from pdf gen")
 }
