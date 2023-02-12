3023aa2749c141792a5fd083a69986ba2d8c59bb
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-02-11 22:18:37 -0300

    PDF for loop basic setup

--- backend/handler/pdfGen.go
+++ backend/handler/pdfGen.go
@@ -2,11 +2,17 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/jung-kurt/gofpdf"
 )
 
+// Get the request body `r` and parses it to JSON
+// The response `w` is a PDF with the contents of the request body if no errors happen
 func HandlePdfGen(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
@@ -33,5 +39,38 @@ func HandlePdfGen(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Hello from pdf gen %v", jsonData)
+	err = generatePdf(jsonData, w)
+
+	if err != nil {
+		fmt.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not send the PDF"))
+	}
+}
+
+// Create a PDF through iterating over `content` and outputs it to `out`
+// Returns errors if any
+func generatePdf(content map[string]interface{}, out io.Writer) error {
+	pdf := gofpdf.New("L", "mm", "A4", "")
+	tr := pdf.UnicodeTranslatorFromDescriptor("")
+	pdf.AddPage()
+	pdf.SetFont("Times", "", 20)
+
+	for k, v := range content {
+		pdf.Write(10, tr(
+			fmt.Sprintf("%v = %v\n", k, v),
+		))
+	}
+
+	if pdf.Err() == true {
+		return errors.New(fmt.Sprintf("Error during PDF creation: %s", pdf.Error()))
+	}
+
+	err := pdf.Output(out)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("Couldn't ouput the PDF to the writer: %s", err))
+	}
+
+	return nil
 }
