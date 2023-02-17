2d30acfc38a2b0abc7e47f16693fcf58d57b1d45
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-02-17 11:42:23 -0300

    Test for the getFormattedString function

--- backend/handler/pdfGen_test.go
+++ backend/handler/pdfGen_test.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"reflect"
+	"sort"
+	"strings"
 	"testing"
 )
 
@@ -25,6 +27,52 @@ func TestGetSliceWithoutNesting(t *testing.T) {
 
 		if reflect.DeepEqual(elmnt.originalSlice, elmnt.expectedSlice) == false {
 			t.Errorf("got %v, wanted %v", elmnt.originalSlice, elmnt.expectedSlice)
+		}
+	}
+}
+
+/*
+Table Driven test for the `getFormattedString` function
+The function is called and the result is split into new lines
+and then the resulting slice is sorted in alphabetical order.
+
+This is done because maps in go are unordered so this workaround allows to test the `getFormattedString` function
+*/
+func TestGetFormattedString(t *testing.T) {
+	/*
+		`arg1` stand for the argument 1 and so on
+		`expected` is the result of calling the function `getFormattedString` and splitting it in new lines and sorting the result slice in alphabetical order
+	*/
+	type getFormattedStringTest struct {
+		arg1       map[string]interface{}
+		arg2, arg3 int
+		expected   []string
+	}
+
+	var getFormattedStringTests = []getFormattedStringTest{
+		{
+			map[string]interface{}{
+				"number1": 1,
+				"number2": 2,
+				"parent": map[string]interface{}{
+					"key": "value",
+				},
+				"parentSlice": []int{1, 2, 3},
+			},
+			4, 3,
+			[]string{"", "       key = value", "   number1 = 1", "   number2 = 2", "   parent:", "   parentSlice = [1 2 3]"},
+		},
+	}
+
+	for _, elmnt := range getFormattedStringTests {
+		result := getFormattedString(elmnt.arg1, elmnt.arg2, elmnt.arg3) // full string result
+
+		got := strings.Split(result, "\n") // splitting the string result in new lines
+
+		sort.Strings(got) // sorting the slice of strings
+
+		if reflect.DeepEqual(got, elmnt.expected) == false {
+			t.Errorf("got %q, wanted %q", got, elmnt.expected)
 		}
 	}
 }
