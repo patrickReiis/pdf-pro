98330d562b7e7c71e9a8b277949aff58879740d3
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-02-22 21:42:20 -0300

    Checking if env variables are set before running the program

--- backend/main.go
+++ backend/main.go
@@ -12,6 +12,27 @@ func main() {
 	if ok != true {
 		log.Fatalf("The env variable %s is not set", "PORT")
 	}
+
+	_, ok = os.LookupEnv("EMAIL")
+	if ok != true {
+		log.Fatalf("The env variable %s is not set", "EMAIL")
+	}
+
+	_, ok = os.LookupEnv("EMAIL_PASSWORD")
+	if ok != true {
+		log.Fatalf("The env variable %s is not set", "EMAIL_PASSWORD")
+	}
+
+	_, ok = os.LookupEnv("EMAIL_HOST")
+	if ok != true {
+		log.Fatalf("The env variable %s is not set", "EMAIL_HOST")
+	}
+
+	_, ok = os.LookupEnv("EMAIL_HOST_PORT")
+	if ok != true {
+		log.Fatalf("The env variable %s is not set", "EMAIL_HOST_PORT")
+	}
+
 	http.HandleFunc("/api/v1/genPdf", handler.HandlePdfGen)
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
