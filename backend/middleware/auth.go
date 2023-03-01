95889744d0de2b5ae6309d7a0c0c81b876593e6c
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-01 20:57:01 -0300

    Basic validation in 'Authorization' Header

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -1,15 +1,23 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 )
 
 // Checks for authorization, if so call the `next` function
 func RouteWithAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
+		authHeaderParts := strings.Fields(authHeader)
 
-		if authHeader == "" {
+		if len(authHeaderParts) != 2 || authHeaderParts[0] != "Secret" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, `{"error":"The VALUE of the 'Authorization' header is not in the right format. The correct format should be: Secret <api-key>"}`)
+
+			return
 		}
 	}
 }
