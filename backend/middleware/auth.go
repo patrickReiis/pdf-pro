ff1e49d2d953158f6d9169c480cba53878a94196
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-08 19:28:48 -0300

    Auth middleware returns a boolean, it's done

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -3,20 +3,34 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"pdfPro/model"
 	"strings"
 )
 
-// Checks for authorization, if so call the `next` function
-func RouteWithAuth(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		authHeader := r.Header.Get("Authorization")
-		authHeaderParts := strings.Fields(authHeader)
+// Acts as a security middleware so it can guarantee that
+// only authorized users can have access to protected routes
+// If the user is authorized the `next` function is called
+func RouteWithAuth(w http.ResponseWriter, r *http.Request) (ok bool) {
+	authHeader := r.Header.Get("Authorization")
+	authHeaderParts := strings.Fields(authHeader)
 
-		if len(authHeaderParts) != 2 || authHeaderParts[0] != "Secret" {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprint(w, `{"error":"The VALUE of the 'Authorization' header is not in the right format. The correct format should be: Secret <api-key>"}`)
-			return
-		}
+	if len(authHeaderParts) != 2 || authHeaderParts[0] != "Secret" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, `{"error":"The VALUE of the 'Authorization' header is not in the right format. The correct format should be: Secret <api-key>"}`)
+		return false
 	}
+
+	apiKey := authHeaderParts[1]
+
+	user := model.GetUserByApiKey(apiKey)
+
+	if user == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, `{"error":"The API KEY you provided is invalid. Check if you are using an API KEY that exists."}`)
+		return false
+	}
+	return true
+
 }
