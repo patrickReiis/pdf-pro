93bc088ed8521cee8f75c34fe9a946b422a2f005
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-15 19:07:09 -0300

    UserAccount schema definition

---
+++ backend/model/entity/UserAccount.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+type JSONB []string
+
+type UserAccount struct {
+	gorm.Model
+	Email             string `gorm:"unique:true"`
+	Password          string
+	RequestsTimestamp JSONB  `gorm:"type:jsonb;default:'[]';not null"`
+	ApiKey            string `gorm:"size:300"`
+}
+
+// Scan Unmarshal
+func (a *JSONB) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if ok == false {
+		return errors.New("type assertion to []byte failed")
+	}
+	return json.Unmarshal(b, a)
+}
+
+// Value Marshal
+func (a JSONB) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
