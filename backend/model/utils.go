b2b2a808fd5ec32f536009e4bc909d9936d84dab
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-19 15:05:48 -0300

    Get api key - wrapper function and function implementation, function to check if the user exists

--- backend/model/utils.go
+++ backend/model/utils.go
@@ -1,9 +1,17 @@
 package model
 
 import (
+	"crypto/rand"
+	"errors"
 	"fmt"
+	"math/big"
+	model "pdfPro/model/entity"
 	"time"
+
+	"gorm.io/gorm"
 )
+
+const ApiKeyLength int = 80
 
 // Returns a slice with the length of 1
 // The element is a timestamp of the time when the this function was called
@@ -15,4 +23,37 @@ func getRequestTimeStamp() []string {
 // Implementation of the `getRequestTimeStamp` function
 func getRequestTimeStampImpl() []string {
 	return []string{fmt.Sprintf(`%s`, time.Now().UTC().Format(time.RFC3339))}
+}
+
+func getRandomApiKey() string {
+	return getRandomApiKeyImpl()
+}
+
+func getRandomApiKeyImpl() string {
+	apiKeyLetters := "abcdefghijklmnopqrstuvwxyz"
+	var max = *big.NewInt(int64(len(apiKeyLetters))) // the maximum number allowed when getting a random number
+	var apiKey string
+
+	for i := 0; i < ApiKeyLength; i++ {
+		randomNumber, _ := rand.Int(rand.Reader, &max) // ignoring potential error
+		apiKey += string(apiKeyLetters[int(randomNumber.Int64())])
+	}
+
+	return apiKey
+}
+
+func doesUserAlreadyExists(email string) (doesUserExists bool) {
+	return doesUserExistsImpl(email)
+}
+
+func doesUserExistsImpl(email string) (doesUserExists bool) {
+	var user model.UserAccount
+	result := dbGorm.Where("email = ?", email).First(&user)
+
+	userNotFound := errors.Is(result.Error, gorm.ErrRecordNotFound)
+	if userNotFound == true {
+		return false
+	}
+
+	return true
 }
