e91a4dc34619bc74a6eb0062ec8f25dc9497e193
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-23 19:41:12 -0300

    The salt will not be returned

--- backend/services/password/hash_test.go
+++ backend/services/password/hash_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestHash(t *testing.T) {
 	plainPassword := "testing321"
-	hashedPassword := Hash(plainPassword) // make hash function return the salt in base64
+	hashedPassword := Hash(plainPassword)
 
 	if hashedPassword == plainPassword {
 		t.Error("Error during password hashing. The hashed password is equal to the plain password.")
