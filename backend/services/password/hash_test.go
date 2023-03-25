99b47c6f2075eaa7b8b37dbbccfe9a1114334be7
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-24 21:09:47 -0300

    test to verify the hash metadata

--- backend/services/password/hash_test.go
+++ backend/services/password/hash_test.go
@@ -12,4 +12,9 @@ func TestHash(t *testing.T) {
 		t.Error("Error during password hashing. The hashed password is equal to the plain password.")
 	}
 
+	desiredHashMetadata := "$argon2id$v=19$m=65536,t=1,p=4$"
+
+	if hashedPassword[:len(desiredHashMetadata)] != desiredHashMetadata {
+		t.Errorf("Expected %s, got %s instead", desiredHashMetadata, hashedPassword[:len(desiredHashMetadata)])
+	}
 }
