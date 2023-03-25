928d3208d17201ca1ceda06a018ce9481677b7fa
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-25 20:20:32 -0300

    Middleware schema is now in a for loop and the route functions are now explicit

--- backend/middleware/middleware.go
+++ backend/middleware/middleware.go
@@ -4,27 +4,25 @@ package middleware
 
 import "net/http"
 
+type middlewareRoute (func(http.ResponseWriter, *http.Request) (ok bool))
+type MiddlewareRoutes []middlewareRoute
+
 // Calls all the midlewares
 // If the request passed all the middleware functions the `originalRoute` will be called
 // Be aware that this function is a Closure.
-func AllMiddleware(originalRoute http.HandlerFunc) http.HandlerFunc {
+func AllMiddleware(originalRoute http.HandlerFunc, middlewares MiddlewareRoutes) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		// If the request doesn't pass through any middleware, the http.ResponseWriter `w` will be modified by writing to it a response.
-		// If `ok` is false it means the http.ResponseWriter `w` WAS modified, therefore should return
-		// If `ok` is true, it means the http.ResponseWriter was NOT modified, therefore proceed the execution
-		ok := RouteWithAuth(w, r)
-		if ok == false {
-			return
-		}
+		for _, middleware := range middlewares {
+			// If `ok` is false it means the http.ResponseWriter `w` WAS modified, therefore should return
+			// If `ok` is true, it means the http.ResponseWriter was NOT modified, therefore proceed the execution
+			ok := middleware(w, r)
 
-		ok = RouteWithRequestSizeLimit(w, r)
-		if ok == false {
-			return
+			if ok == false {
+				return
+			}
 		}
-
 		originalRoute(w, r)
 	}
-
 }
