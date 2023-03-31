1175e6223e570cd16d701f1d05dda9067b948deb
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-03-30 21:32:06 -0300

    Deleting user account by email function definition

--- backend/model/crud.go
+++ backend/model/crud.go
@@ -2,7 +2,9 @@
 
 package model
 
-import model "pdfPro/model/entity"
+import (
+	model "pdfPro/model/entity"
+)
 
 func CreateUserAccount(user *model.UserAccount) (ok bool, err error) {
 	return createUserAccountImpl(user)
@@ -15,4 +17,26 @@ func createUserAccountImpl(user *model.UserAccount) (ok bool, err error) {
 		return false, err
 	}
 	return true, nil
+}
+
+func DeleteUserAccountByEmail(email string) (ok bool, err error) {
+	return deleteUserAccountByEmailImpl(email)
+}
+
+func deleteUserAccountByEmailImpl(email string) (ok bool, err error) {
+
+	var user = &model.UserAccount{Email: email}
+
+	result := dbGorm.Where(user).First(user)
+	if err := result.Error; err != nil {
+		return false, err
+	}
+
+	result = dbGorm.Unscoped().Delete(user)
+	if err = result.Error; err != nil {
+		return false, err
+	}
+
+	return true, nil
+
 }
