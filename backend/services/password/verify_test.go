b7740a1a44205d747514bc35f15fc24cf44367d4
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-04-04 12:28:38 -0300

    New tests with 100% code coverage

--- backend/services/password/verify_test.go
+++ backend/services/password/verify_test.go
@@ -16,7 +16,15 @@ func TestVerify(t *testing.T) {
 		{"plain", "$argon2id$v=20$m=16,t=2,p=1$TlQ4VlFUSTVVOXdYdWRSeg$DOPiC1dzuCWQ8fp20U7hKQ", false},
 		{"plain", "$argon2id$v=19$m=16,t=4,p=1$cHhMQzNramZKMmhud1VSVA$mkFEIXUkAagVsHNZ2jgJvjwEHOuLsKk6", true},
 		{"very long password bla bla bla", "$argon2id$v=19$m=40000,t=14,p=3$a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", true},
-		{"very long password bla bla bla", "$argon2d$v=19$m=40000,t=14,p=3$a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", false},
+		{"very long password bla bla bla", "argon2d$v=19$m=40000,t=14,p=3$a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", false},
+		{"very long password bla bla bla", "argon2d$v=19$m=40000,t=not number,=3$a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", false},
+		{"very long password bla bla bla", "$argon2id$v=19$m=40000,t=14,p=3$ç~´´´a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", false},
+		{"very long password bla bla bla", "argon2d$v=19$m=40000,t=14,p=3$a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFç]]ç~KGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", false},
+		{"very long password bla bla bla", "$argon2id$v=19$m=40000,t=14,p=3$a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Yb", false},
+		{"very long password bla bla bla", "$argon2i$v=19$m=40000,t=14,p=3$a0ZvNFdnZnpOZDEwTGNtZA$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", false},
+		{"plain", "$argon2id$v=19$f=false$cHhMQzNramZKMmhud1VSVA$mkFEIXUkAagVsHNZ2jgJvjwEHOuLsKk6", false},
+		{"very long password bla bla bla", "$argon2id$v=19$m=40000,t=14,p=3$a0ZvNFdnZnpOZDEwTG$nWMoW4vrZFKGvAI5z6TB4GXOqIDptyos1yTW1Ybykm4", false},
+		{"plain", "$argon2id$v=19$m=16,t=2,p=1$R0xKclFSQ3VmRUhUTlNNag$cRR+6shII+S2fRR1Tp8XqA", false},
 	}
 
 	for _, e := range testCases {
