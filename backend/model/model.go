5753612c362522de51cefc8ea35a33256d03bcdc
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-04-05 12:00:05 -0300

    moved function to a better file for coherence

--- backend/model/model.go
+++ backend/model/model.go
@@ -34,13 +34,3 @@ func connectDb() (*gorm.DB, error) {
 
 	return db, nil
 }
-
-// Returns the user by its Api Key
-// If the user does not exists returns an empty string
-func GetUserByApiKey(apiKey string) (user string) {
-	return getUserByApiKeyImpl(apiKey)
-}
-
-func getUserByApiKeyImpl(apiKey string) string {
-	return "implement"
-}
--- backend/model/utils.go
+++ backend/model/utils.go
@@ -58,3 +58,13 @@ func doesUserExistsImpl(email string) (doesUserExists bool) {
 
 	return true
 }
+
+// Returns the user by its Api Key
+// If the user does not exists returns an empty string
+func GetUserByApiKey(apiKey string) (user string) {
+	return getUserByApiKeyImpl(apiKey)
+}
+
+func getUserByApiKeyImpl(apiKey string) string {
+	return "implement"
+}
