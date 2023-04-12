cd4a7916a585353a74faf8fb8489d8144ba23a35
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-04-12 11:41:41 -0300

    Error handling and get secret key as env variable

--- backend/services/authJwt/sign.go
+++ backend/services/authJwt/sign.go
@@ -2,6 +2,7 @@ package authJwt
 
 import (
 	"encoding/json"
+	"pdfPro/model"
 
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -14,12 +15,24 @@ func signImpl(payload json.RawMessage) (string, error) {
 	var data jwt.MapClaims
 
 	err := json.Unmarshal(payload, &data)
+	if err != nil {
+		return "", nil
+	}
 
 	// Create token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, data)
 
-	// Sign the token with a secret key
-	tokenString, err := token.SignedString([]byte("secret key sample, get another password through a new env variable later"))
+	// Get secret key
+	key, err := model.GetJwtSecret()
+	if err != nil {
+		return "", nil
+	}
 
-	return tokenString, err
+	// Sign the token with a secret key
+	tokenString, err := token.SignedString([]byte(key))
+	if err != nil {
+		return "", nil
+	}
+
+	return tokenString, nil
 }
