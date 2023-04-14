86c36a2009f71aa60d93ba1cc8469e6e03a5ac19
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-04-14 13:07:13 -0300

    Better test coverage: 84.6%

--- backend/services/authJwt/sign_test.go
+++ backend/services/authJwt/sign_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 func TestSign(t *testing.T) {
+	// Tests 2 possible scenarios, the first case should be correct and no errors should happen
+	// The second case should return return an error since a string will try to be parsed into a JSON object
 
+	// ------------ FIRST CASE
 	// Current UTC time + 2 hours
 	exp := time.Now().UTC().Add(time.Hour * 2)
 
@@ -21,6 +24,17 @@ func TestSign(t *testing.T) {
 	_, err = Sign(payload)
 	if err != nil {
 		t.Error(err)
+	}
+
+	// ------------ SECOND CASE
+	payload, err = json.Marshal("string")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = Sign(payload)
+	if err == nil {
+		t.Error("Should have returned the error message: cannot unmarshal string into Go value of type jwt.MapClaims")
 	}
 
 }
