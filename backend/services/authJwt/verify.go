ccda1258b1baa88a8880a0b429abedebf71f7c36
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-04-16 17:02:12 -0300

    Func to verify a JWT token

--- backend/services/authJwt/verify.go
+++ backend/services/authJwt/verify.go
@@ -1,8 +1,14 @@
 package authJwt
 
+import "github.com/golang-jwt/jwt/v4"
+
 // Verifies wheter the jwt `token` is valid or not
-func Verify() {
+func Verify(token, secretKey string) (*jwt.Token, error) {
+	return verifyImpl(token, secretKey)
 }
 
-func verifyImpl() {
+func verifyImpl(token, secretKey string) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(token, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(secretKey), nil
+	})
 }
