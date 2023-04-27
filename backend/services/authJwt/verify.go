f1b8b1e1783bd6fcb65fa92c002ff47552eac22b
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-04-27 11:55:50 -0300

    Adding a custom claim for when verifying and parsing a JWT

--- backend/services/authJwt/verify.go
+++ backend/services/authJwt/verify.go
@@ -3,12 +3,15 @@ package authJwt
 import "github.com/golang-jwt/jwt/v4"
 
 // Verifies wheter the jwt `token` is valid or not
-func Verify(token, secretKey string) (*jwt.Token, error) {
-	return verifyImpl(token, secretKey)
+// `customClaims` is used for parsing the JWT claims (registered claims or user defined claims) in a simple way
+// `customClaims` needs to be a pointer otherwise a panic will happen
+// See https://pkg.go.dev/github.com/golang-jwt/jwt/v4#ParseWithClaims
+func Verify(token string, customClaims jwt.Claims, secretKey string) (*jwt.Token, error) {
+	return verifyImpl(token, customClaims, secretKey)
 }
 
-func verifyImpl(token, secretKey string) (*jwt.Token, error) {
-	return jwt.ParseWithClaims(token, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
+func verifyImpl(token string, customClaims jwt.Claims, secretKey string) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(token, customClaims, func(t *jwt.Token) (interface{}, error) {
 		return []byte(secretKey), nil
 	})
 }
