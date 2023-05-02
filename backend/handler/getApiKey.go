cfc38004879f6c4ed6aaa6c2fe73820a0cef3a83
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-05-02 12:19:50 -0300

    Get user api key endpoint, function definiton

--- backend/handler/getApiKey.go
+++ backend/handler/getApiKey.go
@@ -1,5 +1,33 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"pdfPro/model"
+	"pdfPro/services/authJwt"
+	"strings"
+)
 
-func HandleGetApiKey(w http.ResponseWriter, r *http.Request) {}
+func HandleGetApiKey(w http.ResponseWriter, r *http.Request) {
+	// There is no validation here since the authen middleware already handles that
+	authHeader := r.Header.Get("Authorization")
+	authHeaderParts := strings.Fields(authHeader)
+
+	secret, _ := model.GetJwtSecret()
+
+	token, _ := authJwt.Verify(authHeaderParts[1], &authJwt.CustomClaims{}, secret)
+	// type assertion
+	claims, _ := token.Claims.(*authJwt.CustomClaims)
+
+	apiKey, err := model.GetUserApiKey(claims.Email)
+	if err != nil || apiKey == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, `{"error":"We couldn't load your API_KEY"}`)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `{"apiKey":"%s"}`, apiKey)
+}
