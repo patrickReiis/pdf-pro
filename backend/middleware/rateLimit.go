019886104bee717ba2315d870c7042d4286e3f86
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-05-22 11:47:26 -0300

    Rate limit basic setup with instructions

--- backend/middleware/rateLimit.go
+++ backend/middleware/rateLimit.go
@@ -1,7 +1,37 @@
 package middleware
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"pdfPro/model"
+	"strings"
+)
 
-func RouteWithRateLimiting(w http.ResponseWriter, r *http.Request) {
+func RouteWithRateLimiting(w http.ResponseWriter, r *http.Request) (ok bool) {
+	authHeader := r.Header.Get("Authorization")
+	authHeaderParts := strings.Fields(authHeader)
 
+	if len(authHeaderParts) != 2 || authHeaderParts[0] != "Secret" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, `{"error":"The VALUE of the 'Authorization' header is not in the right format. The correct format should be: Secret <api-key>"}`)
+		return
+	}
+
+	apiKey := authHeaderParts[1]
+
+	timestamps, err := model.GetUserTimestampByApiKey(apiKey)
+	if err != nil {
+		fmt.Fprint(w, "not found")
+		return false
+	}
+
+	fmt.Println(timestamps)
+	// If you want to implement this middleware:
+
+	model.GetRequestTimeStamp() // call this function to get a list of dates
+	// Compare if the request is allowed. For example if it's 1000 requests per hour you can check that
+	model.UpdateUserTimestamp() // Go inside this function to implement a mechanism to increment a new timestamp
+
+	return false // return true
 }
